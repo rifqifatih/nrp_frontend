e0a78b9362f14ae70ff3f72b220f0822032747b2
Rifqi Al Fatih <rifqialfatih@gmail.com>
2020-05-29 09:11:14 +0200

    Replace NodePort

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,8 @@
 export NODE_IP=$(curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
 
 source /home/bbpnrsoa/nrp/src/user-scripts/nrp_variables
-/bin/sed -e 's/localhost/'"$NODE_IP"'/' -i /home/bbpnrsoa/nrp/src/ExDFrontend/dist/config.json
+/bin/sed -e 's/localhost:9000/'"$NODE_IP"':30000/' -i /home/bbpnrsoa/nrp/src/ExDFrontend/dist/config.json
 /bin/sed -e 's/localhost/'"$NODE_IP"'/' -i /home/bbpnrsoa/nrp/src/nrpBackendProxy/config.json
+/bin/sed -e 's/8080/30001/' -i /home/bbpnrsoa/nrp/src/nrpBackendProxy/config.json
 
 sudo /usr/bin/supervisord -n
