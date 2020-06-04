8c2a90c6ed6a6489bc6a9e9ee2a0e06b1a06f834
Rifqi Al Fatih <rifqialfatih@gmail.com>
2020-06-04 20:44:29 +0200

    Entrypoint update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@
 export NODE_IP=$(curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
 
 source /home/bbpnrsoa/nrp/src/user-scripts/nrp_variables
+sudo chown -R bbpnrsoa:bbp-ext /home/bbpnrsoa/nrp/src/Experiments && sudo chown -R bbpnrsoa:bbp-ext /home/bbpnrsoa/nrp/src/Models
 # For kubernetes
 /bin/sed -e 's/localhost:9000/'"$NODE_IP"':30000/' -i /home/bbpnrsoa/nrp/src/ExDFrontend/dist/config.json
 /bin/sed -e 's/localhost/'"$NODE_IP"'/' -i /home/bbpnrsoa/nrp/src/nrpBackendProxy/config.json
